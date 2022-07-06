e319639518b4cb2931c910a6db2583eaec70c2fd
JangIBin <59593043+JangIBin@users.noreply.github.com>
2022-07-06 14:41:56 +0900

    0706

---
+++ Day0705/src/com/awtex/FileDialogEx.java
@@ -0,0 +1,26 @@
+package com.awtex;
+import java.awt.*;
+
+public class FileDialogEx extends Frame {
+	
+	FileDialog fd = new FileDialog(this, "내꺼저장", FileDialog.SAVE);
+	
+	public FileDialogEx() {
+		super("FileDialog 테스트");
+		
+//		WinEvent we = new WinEvent();
+//		addWindowListener(we);
+		setSize(300, 200);
+		setVisible(true);
+		fd.setVisible(true);
+		System.out.println(fd.getFile());
+		
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
